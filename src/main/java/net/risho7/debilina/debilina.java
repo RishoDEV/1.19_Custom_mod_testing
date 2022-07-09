cd78bba40c57b8b90b3d6d35fa10403fd1c78f48
RishoDEV <citruskysli@gmail.com>
2022-07-10 01:19:09 +0200

    Initial commit

---
+++ src/main/java/net/risho7/debilina/debilina.java
@@ -0,0 +1,15 @@
+package net.risho7.debilina;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class debilina implements ModInitializer {
+	public static final String MOD_ID = "debilina";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
